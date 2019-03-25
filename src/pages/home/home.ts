b8ef9a5ac6805344733b59ac45932ec0628efdca
Angelo <angelocanarion@hotmail.com>
2019-03-25 21:53:17 +0000

    Solucionado problema mostrar anuncios

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,30 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy} from '@angular/core';
 import { IonicPage } from 'ionic-angular';
 import {MeetingI} from  '../../app/models/meeting.interface';
 import {AnuncioProvider} from '../../providers/anuncio'
 import { NavController } from 'ionic-angular';
 import {AnuncioDetailsPage} from '../anuncio-details/anuncio-details';
+import { Subscription  } from 'rxjs/Subscription';
 
 @IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage implements OnInit{
+export class HomePage implements  OnInit{
   anuncios: MeetingI[];
+   observer: Subscription ;
   constructor(private anuncioService:AnuncioProvider, private navCtrl: NavController){}
 
+  ionViewCanLeave(){
+      this.observer.unsubscribe();
+  }
   ngOnInit(){
-    this.anuncioService.getAnuncios().subscribe(res =>{
+    this.observer = this.anuncioService.getAnuncios().subscribe(res =>{
       this.anuncios = res;
     });
   }
 
-  ionViewWillEnter(){
-    this.anuncioService.getAnuncios().subscribe(res =>{
-      this.anuncios = res;
-    });
-  }
   abrirAnuncioDetails(ident) {
     this.navCtrl.push(AnuncioDetailsPage,{id: ident});
   }
